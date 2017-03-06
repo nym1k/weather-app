71d483d49fa5ffbe7d8148d68bddc62fe505fc02
Ross King <sleeping.stars@gmail.com>
2017-03-06 12:08:00 +0000

    Rearranged and tidied HTML, restricted container size and added background to wrap, added hover effects to container, moved location and temp to top of container

--- app.js
+++ app.js
@@ -7,21 +7,25 @@ $(function(){
       },
       getLocation: function() {
         var location = "";
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(App.getWeather);
-        } else {
-          $('#summary').text("Not supported");
-        }
+        $.get("http://ipinfo.io", function(response) {
+          $('#location').text(response.city + ', ' + response.region + ', ' + response.country);
+          App.getWeather(response.loc);
+        }, "jsonp")
+        // if (navigator.geolocation) {
+        //   navigator.geolocation.getCurrentPosition(App.getWeather);
+        // } else {
+        //   $('#summary').text("Not supported");
+        // }
       },
       getWeather: function(position) {
-        var location = position.coords.latitude + "," + position.coords.longitude;
-        var forcastURL = "https://api.forecast.io/forecast/26a6920c013eecb823f107f10098a3ce/" + location + "?units=si&callback=?";
+        var forcastURL = "https://api.forecast.io/forecast/26a6920c013eecb823f107f10098a3ce/" + position + "?units=si&callback=?";
 
         $.getJSON(forcastURL, function(json) {
           var data = json.currently;
+          $('#icon').html('<img src="img/' + json.currently.icon + '.svg"/>');
           $('#temp').text((Math.round(data.temperature)));
           $('#summary').text(data.summary);
-          $('.container').css("background-image", "url('" + App.bgs[json.currently.icon] + "')");
+          $('.container').css("background-image", "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url('img/" + json.currently.icon + ".jpg')");
         });
       },
       bindChangeUnits: function() {
@@ -47,19 +51,7 @@ $(function(){
           //update symbol
           $("#units").text("C");
         }
-      },
-      bgs: {
-        "clear-day" : "http://www.mrwallpaper.com/wallpapers/sunny-day-landscape-1920x1080.jpg",
-				"clear-night" : "http://i.imgur.com/HF3Xxg1.jpg",
-				"rain" : "http://onehdwallpaper.com/wp-content/uploads/2015/06/Rainy-Night-Desktop-Wallpapers.jpg",
-				"snow" : "https://wallpaperscraft.com/image/city_street_snow_winter_lane_black_white_28527_1920x1080.jpg",
-				"sleet" : "http://media.wktv.com/images/snow+tires+icy+road+car.jpg",
-				"wind" : "https://i.ytimg.com/vi/NhzwezPvwjk/maxresdefault.jpg",
-				"fog" : "http://hdimagesnew.com/wp-content/uploads/2015/11/Fog-Wallpapers-HD-6.jpg",
-				"cloudy" : "http://img.wallpaperfolder.com/f/798EBE3CA994/cloudy-sky-over-field-7480.jpg",
-				"partly-cloudy-day" : "http://1920x1080hdwallpapers.com/image/201510/nature/2448/lake-calm-overcast-forest-reflection-austria.jpg",
-				"partly-cloudy-night" : "https://i.ytimg.com/vi/Xw_k7duWXMY/maxresdefault.jpg"
-			}
+      }
     };
 
 App.Init();
